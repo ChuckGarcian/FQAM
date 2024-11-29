3940f8f82c0b98f8546444ebb87088b070828086
Chuck Garcia <chuckgarcian@gmail.com>
2024-11-29 15:17:33 -0600

    update rendering to image only instead of realtime

--- src/FQAM_Main.c
+++ src/FQAM_Main.c
@@ -19,7 +19,7 @@ static bool _FQAM_initialized = false;
 static struct stage {
   FLA_Obj statevector;   // Quantum statevector 
   size_t dim;            // Dimension of hilbertspace
-  size_t state_size;     // Statevector size
+  size_t state_space;     // Statevector size
   arraylist *stage;      // Contain sequence applied computation steps
 } main_stage;
 
@@ -39,7 +39,7 @@ void FQAM_init (size_t dim, unsigned int initial_state)
     
     // Initialize Statevector buffer
     dcomplex *buf;
-    int state_size = pow (2, dim);
+    int state_space = pow (2, dim);
     
     buf = (dcomplex *) calloc (sizeof (dcomplex), pow (2, dim));
     assertf (buf, "Error: Failed to allocate statevector");
@@ -48,10 +48,10 @@ void FQAM_init (size_t dim, unsigned int initial_state)
     buf[initial_state].imag = 0.0;
 
     // Initialize Stage 
-    FLA_Obj_create_without_buffer (FLA_DOUBLE_COMPLEX, state_size, 1, &main_stage.statevector); 
-    FLA_Obj_attach_buffer (buf, 1, state_size, &main_stage.statevector);   
+    FLA_Obj_create_without_buffer (FLA_DOUBLE_COMPLEX, state_space, 1, &main_stage.statevector); 
+    FLA_Obj_attach_buffer (buf, 1, state_space, &main_stage.statevector);   
 
-    main_stage.state_size = state_size;
+    main_stage.state_space = state_space;
     main_stage.dim        = dim;
     main_stage.stage      = arraylist_create ();
     _FQAM_initialized     = true;
@@ -123,11 +123,9 @@ bool FQAM_initialized (void)
 }
 
 /* Applys operator to state vector */
-void apply_operator(FQAM_Op *op)
+void apply_operator(FLA_Obj A)
 {
-    FLA_Obj A, x, y_tmp;
-
-    A = op->mat_repr;
+    FLA_Obj x, y_tmp;
     x = main_stage.statevector;
     FLA_Obj_create_conf_to(FLA_NO_TRANSPOSE, x, &y_tmp); // Temp
 
@@ -148,7 +146,7 @@ void FQAM_compute_outcomes (void)
     for (int idx = 0; idx < main_stage.stage->size; idx++)
     {
         FQAM_Op *operator = arraylist_get (main_stage.stage, idx);
-        apply_operator (operator);
+        apply_operator (operator->mat_repr);
     }
 }
 
@@ -214,96 +212,192 @@ void _debug_show_state_data (void)
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
 
-// #define MAX_RECS_X 4
-// #define MAX_RECS_Y 2
+int gen_path_probility_matrix ( FLA_Obj A, FLA_Obj bt, FLA_Obj C );
 
-#define PLAY_TIME_IN_FRAMES 240 // At 60 fps = 4 seconds
-
-/* Stage Layer struct for rendering */
-struct Stage_Layer {  
-  Rectangle *recs; 
-  Color color;
-} layer_recs;
-
-void init_grid (struct Stage_Layer *layer_recs, int max_recs_x, int max_recs_y);
-void color_grid (struct Stage_Layer *layer_recs, int layer_idx);
+void draw_transition_lines (Image *image, FLA_Obj adjacency_matrix, int time_step, const int spacing_x, const int spacing_y);
+void draw_next_state (Image *image, FLA_Obj state, int time_step, const int spacing_x, const int spacing_y);
 
 FQAM_Error FQAM_show_diagram (void)
-{
+{  
   assertf (FQAM_initialized (), "Error: Expected core initialized");
   
-  const int screenWidth = SCREEN_WIDTH;
-  const int screenHeight = SCREEN_HEIGHT;
-
-  int max_recs_x = main_stage.stage->size + 1;
-  int max_recs_y = main_stage.state_size;
+  int screenWidth, screenHeight; 
+  const int spacing_x, spacing_y;
+  float rotation;
   
-  struct Stage_Layer layers = {0};
-  layers.recs = (Rectangle *) calloc (sizeof (Rectangle), max_recs_x * max_recs_y);
-  layers.color = BLUE; // Initial Color
+  FLA_Obj adjacency_matrix;  
 
-  InitWindow (screenWidth, screenHeight,
-              "raylib [shapes] example - easings rectangle array");
-  printf ("Max_recs_x: %d \n", max_recs_x);
-  printf ("Max_recs_y: %d \n", max_recs_y);    
-  init_grid (&layers, max_recs_x, max_recs_y);
+  screenWidth  = SCREEN_WIDTH;
+  screenHeight = SCREEN_HEIGHT;
+  
+  dim_t input_states, output_states;    
+  Image result_image;
 
-  float rotation = 0.0f;
-  int framesCounter = 0;
-
-  SetTargetFPS (60);
-  int idx = 0;
+  // Create result image object
+  result_image = GenImageColor (screenWidth, screenHeight, RED);    
     
+  // Input states are rows and columns outputs
+  input_states = main_stage.state_space;
+  output_states = main_stage.state_space;
+  
+  // Create adjacency matrix buffer and set to zeros
+  FLA_Obj_create (FLA_DOUBLE_COMPLEX, input_states, output_states, 0, 0, &adjacency_matrix);
+  FLA_set (FLA_ZERO, adjacency_matrix);
 
-  while (!WindowShouldClose ())
-  {
+  // Compute and draw transition probabilities 
+  for (int time_step = 0; time_step < main_stage.stage->size; )
+  {    
+    FLA_Obj operator, state, adjacency_matrix;
     
-    // if (idx < main_stage.stage->size)
-    // {
-    //   FQAM_Op *operator = arraylist_get (main_stage.stage, idx);
-    //   apply_operator (operator);
-    //   color_grid (&layer_recs, idx);
-    // }
+    // Compute next state and adjacency matrix
+    operator = ((FQAM_Op *) arraylist_get (main_stage.stage, time_step))->mat_repr;
+    state = main_stage.statevector;
     
-    BeginDrawing ();
-    ClearBackground (RAYWHITE);
-
+    if (time_step) // Fence post
+      gen_path_probility_matrix (operator, state, adjacency_matrix);
+    apply_operator (operator);
     
-    for (int i = 0; i < max_recs_x * max_recs_y; i++)
-    {
-      DrawRectanglePro (layers.recs[i], (Vector2){layers.recs[i].width, layers.recs[i].height}, rotation,
-                        layers.color);
-    }
+    // Draw next state and adjacency matrix
+    draw_next_state (&result_image, state, time_step, spacing_x, spacing_y);
+    draw_transition_lines     (&result_image, adjacency_matrix, time_step, spacing_x, spacing_y);            
+  }  
 
-    EndDrawing ();
-  }
-
-  CloseWindow ();
-
+  ExportImage (result_image, "saved_image.png");
+  UnloadImage (result_image);
   return 0;
 }
 
-void init_grid (struct Stage_Layer *layer_recs, int max_recs_x, int max_recs_y)
+/* Copyright 2024 The University of Texas at Austin  
+ 
+   For licensing information see
+                  http://www.cs.utexas.edu/users/flame/license.html 
+
+   Programmed by: Name of author
+                  Email of author
+                                                                     */
+
+/*
+Arguments:
+  FLA_Obj A: Operator Matrix
+  FLA_Obj bt: current state vector as row vector
+    FLA_Obj C: Path probability adjacency matrix. Matrix specifying the probability
+     amplitudes of edges connecting the i-th and (i + 1)-th state.
+*/
+int gen_path_probility_matrix ( FLA_Obj A, FLA_Obj bt, FLA_Obj C )
 {
-  float spacingX = (SCREEN_WIDTH - (max_recs_x * RECS_WIDTH)) / max_recs_x;
-  float spacingY = (SCREEN_HEIGHT - (max_recs_y * RECS_HEIGHT)) / max_recs_y;
+  FLA_Obj AL,    AR,       A0,  a1,  A2;
 
-  assertf (spacingX > 0, "Error: Unhandled negative spacing error");
-  assertf (spacingY > 0, "Error: Unhandled negative spacing error");
+  FLA_Obj bLt,    bRt,       b0t,  beta1,  b2t;
 
-  for (int y = 0; y < max_recs_y; y++)
-  {
-    for (int x = 0; x < max_recs_x; x++)
-    {
-      layer_recs->recs[y * max_recs_x + x].x = ((RECS_WIDTH + spacingX) * x) + RECS_WIDTH * 2;
-      layer_recs->recs[y * max_recs_x + x].y = ((RECS_HEIGHT + spacingY) * y) + RECS_HEIGHT * 2;
-      layer_recs->recs[y * max_recs_x + x].width = RECS_WIDTH;
-      layer_recs->recs[y * max_recs_x + x].height = RECS_HEIGHT;
-    }
+  FLA_Obj CT,              C0,
+          CB,              c1t,
+                           C2;
+
+  FLA_Part_1x2( A,    &AL,  &AR,      0, FLA_LEFT );
+
+  FLA_Part_1x2( bt,    &bLt,  &bRt,      0, FLA_LEFT );
+
+  FLA_Part_2x1( C,    &CT, 
+                      &CB,            0, FLA_TOP );
+
+  while ( FLA_Obj_width( AL ) < FLA_Obj_width( A ) ){
+
+    FLA_Repart_1x2_to_1x3( AL,  /**/ AR,        &A0, /**/ &a1, &A2,
+                           1, FLA_RIGHT );
+
+    FLA_Repart_1x2_to_1x3( bLt,  /**/ bRt,        &b0t, /**/ &beta1, &b2t,
+                           1, FLA_RIGHT );
+
+    FLA_Repart_2x1_to_3x1( CT,                &C0, 
+                        /* ** */            /* *** */
+                                              &c1t, 
+                           CB,                &C2,        1, FLA_BOTTOM );
+
+
+    // Update 
+    print ("c1t Length: %d", FLA_Obj_length (c1t));
+    print ("c1t Width: %d", FLA_Obj_width (c1t));
+    // FLA_Axpy (FLA_TRANSPOSE, beta1, a1, c1t);
+    
+
+    FLA_Cont_with_1x3_to_1x2( &AL,  /**/ &AR,        A0, a1, /**/ A2,
+                              FLA_LEFT );
+
+    FLA_Cont_with_1x3_to_1x2( &bLt,  /**/ &bRt,        b0t, beta1, /**/ b2t,
+                              FLA_LEFT );
+
+    FLA_Cont_with_3x1_to_2x1( &CT,                C0, 
+                                                  c1t, 
+                            /* ** */           /* *** */
+                              &CB,                C2,     FLA_TOP );
+
   }
+
+  return FLA_SUCCESS;
 }
 
-// void color_grid (struct Stage_Layer *layer_recs, int layer_idx)
+void draw_transition_lines (Image *image, FLA_Obj adjacency_matrix, int time_step, const int spacing_x, const int spacing_y)
+{
+  int max_recs_x, max_recs_y;
+  int length, width;
+  
+  max_recs_x = FLA_Obj_length (adjacency_matrix);
+  
+  length = FLA_Obj_length (adjacency_matrix);
+  width  = FLA_Obj_width (adjacency_matrix);
+  
+  // float spacingX = (SCREEN_WIDTH - (max_recs_x * RECS_WIDTH)) / max_recs_x;
+  float spacingY = (SCREEN_HEIGHT - (max_recs_y * RECS_HEIGHT)) / max_recs_y;
+
+  // assertf (spacingX > 0, "Error: Unhandled negative spacing error");
+  // assertf (spacingY > 0, "Error: Unhandled negative spacing error");
+
+  BeginDrawing ();
+
+
+  for (int x = 0; x < length; x++)
+  {
+    layer_recs->recs[y * max_recs_x + x].x = ((RECS_WIDTH + spacing_x) * x) + RECS_WIDTH * 2;
+    layer_recs->recs[y * max_recs_x + x].y = ((RECS_HEIGHT + spacing_y) * y) + RECS_HEIGHT * 2;
+    layer_recs->recs[y * max_recs_x + x].width = RECS_WIDTH;
+    layer_recs->recs[y * max_recs_x + x].height = RECS_HEIGHT;
+    
+    if (main_stage.stage)
+      layer_recs->color = GRAY
+  }
+  }    
+  
+  for (int i = 0; i < max_recs_x * max_recs_y; i++)
+  {
+    DrawRectanglePro (layers.recs[i], (Vector2){layers.recs[i].width, layers.recs[i].height}, rotation,
+                      layers.color);
+  }
+
+}
+
+void draw_next_state (Image *image, FLA_Obj state, int time_step, const int spacing_x, const int spacing_y)
+{
+  
+  int width  = FLA_Obj_width (state)    
+  
+  // Render each basis state 
+  for (int y = 0; y < width; y++)
+  {
+    Rectangle rec;
+    
+    rec.x      = ((RECS_WIDTH + spacingX) * time_step) + RECS_WIDTH * 2;
+    rec.y      = ((RECS_HEIGHT + spacingY) * y) + RECS_HEIGHT * 2;
+    rec.width  = RECS_WIDTH;
+    rec.height = RECS_HEIGHT;    
+    
+    Color color = RED;    
+    
+    DrawRectanglePro (rec, (Vector2){rec.width, rec.height}, rotation, color);
+    }
+}    
+
+
+// void color_grid (struct Stage_Layer *layer_recs, int layer_idx, )
 // {
 //   for (int y = 0; y < MAX_RECS_Y; y++) 
 //   {
